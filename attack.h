d2dbf2e694011a6effb49ff37f93f898147adab8
Noscope999 <sahay.shikhar@gmail.com>
2026-03-03 02:30:22 +0530

    Added attack file

---
+++ attack.h
@@ -0,0 +1,45 @@
+#ifndef ATTACK_H
+#define ATTACK_H
+
+#include <systemc.h>
+#include "network.h"
+
+SC_MODULE(AttackInjector) {
+
+    Network* net;
+
+    void inject() {
+
+        wait(50, SC_MS);
+
+        // Spoofing attack
+        Message fake;
+        fake.sender = 99;
+        fake.data = 999;
+        fake.timestamp = sc_time_stamp();
+
+        std::cout << "🔥 Spoofing Attack Injected\n";
+        net->transmit(fake);
+
+        wait(50, SC_MS);
+
+        // DoS attack
+        std::cout << "🔥 DoS Attack Injected\n";
+        for (int i = 0; i < 30; i++) {
+            Message flood;
+            flood.sender = 99;
+            flood.data = rand();
+            flood.timestamp = sc_time_stamp();
+            net->transmit(flood);
+        }
+    }
+
+    SC_HAS_PROCESS(AttackInjector);
+
+    AttackInjector(sc_module_name name, Network* network)
+        : sc_module(name), net(network) {
+        SC_THREAD(inject);
+    }
+};
+
+#endif
